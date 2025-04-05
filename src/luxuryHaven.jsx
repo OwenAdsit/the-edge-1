f7ad0ad9eb7c1231c8deec7d947a0a515dc87ae9
sujan <asujan205@gmail.com>
2025-04-05 12:12:14 +0545

    feat:fixed-typography-and-things

--- src/luxuryHaven.jsx
+++ src/luxuryHaven.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const LuxuryHaven = () => {
   return (
     <div className="flex justify-center items-center min-h-screen app-container">
-      <div className="max-w-3xl text-center">
+      <div className=" max-w-lg sm:max-w-2xl lg:max-w-3xl  text-center">
         <h2 className="display-5 font-normal leading-snug text-start">
           A secluded haven of luxury above the Indian Ocean, where endless ocean
           views merge seamlessly with the horizon.
         </h2>
-        <p className="mt-4 text-gray-600 text-body leading-relaxed text-start">
+        <p className="mt-4  text-body leading-relaxed text-start">
           Perched atop Uluwatu’s cliffs, the villas at The edge offer unmatched
           privacy and breathtaking ocean views. A perfect blend of modern luxury
           and traditional charm, each villa serves as your own personal
